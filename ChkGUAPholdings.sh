5202b4fecf6a17bdb3aec8ee7a9a4838aad15a14
Jason Lamb <jlamb@lambcorp.com>
2019-09-07 18:45:12 -0400

    Update ChkGUAPholdings.sh

--- ChkGUAPholdings.sh
+++ ChkGUAPholdings.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-MN1= &(curl http://159.65.221.180:3001/ext/getbalance/GK6Vdaq4Gp7G5aUSPiMFb6gKpk3Sq5xeCs)
+MN1= $(curl http://159.65.221.180:3001/ext/getbalance/GK6Vdaq4Gp7G5aUSPiMFb6gKpk3Sq5xeCs)
 
 echo "MN1 Total= $MN1"
 
-MN2= curl http://159.65.221.180:3001/ext/getbalance/GPRN7jMA9DAKPaSThT9TpH3EsHfgG2zSg6
+MN2= $(curl http://159.65.221.180:3001/ext/getbalance/GPRN7jMA9DAKPaSThT9TpH3EsHfgG2zSg6)
 
 echo ""
 echo "MN2 Total= $MN2"
 
-MN3= curl http://159.65.221.180:3001/ext/getbalance/GadrCsoWB1YoGKiAofte1c8iyNaZpn7DHR
+MN3= $(curl http://159.65.221.180:3001/ext/getbalance/GadrCsoWB1YoGKiAofte1c8iyNaZpn7DHR)
 
 echo ""
 echo "MN3 Total= $MN3"
 
-MN4= curl  http://159.65.221.180:3001/ext/getbalance/GVmzuuU8M2epADTHAFTcJfKKeVAT4odyZh
+MN4= $(curl http://159.65.221.180:3001/ext/getbalance/GVmzuuU8M2epADTHAFTcJfKKeVAT4odyZh)
 
 echo ""
 echo "MN4 Total= $MN4"
 
-MN5=  curl http://159.65.221.180:3001/ext/getbalance/GTsz49S1dgbMcC82wcekku6kyHwNU97yYa
+MN5=  $(curl http://159.65.221.180:3001/ext/getbalance/GTsz49S1dgbMcC82wcekku6kyHwNU97yYa)
 
 echo ""
 echo "MN5 Total= $MN5"
 
-MN6= curl http://159.65.221.180:3001/ext/getbalance/GNENXQeidRkFxfjjoRcNiLpfSRMvs5eY6d
+MN6= $(curl http://159.65.221.180:3001/ext/getbalance/GNENXQeidRkFxfjjoRcNiLpfSRMvs5eY6d)
 
 echo ""
 echo "MN6 Total= $MN6"
