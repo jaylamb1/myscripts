ce515cabbf51b7ef9300dd880bc944f13543a235
Jason Lamb <jlamb@lambcorp.com>
2019-09-09 16:27:57 -0400

    Update ChkGUAPholdings.sh

--- ChkGUAPholdings.sh
+++ ChkGUAPholdings.sh
@@ -48,7 +48,7 @@ GUAPTotal=$(curl -s -X GET $parm7)
 
 GUAPTotal=${GUAPTotal%%.*}
 
-Perc=`echo - | awk '{print MNTotal / GUAPTotal}'`
+Perc=`echo - | awk '{print $MNTotal / $GUAPTotal}'`
 #Perc=$(( '$MNTotal / $GUAPTotal' | bc -l ))
 
 echo "Total GUAP Holdings for all MNs: $MNTotal"
