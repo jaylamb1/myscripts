bdc40a52d8245893b8cffe053aa692d53c1a6c7a
Jason Lamb <jlamb@lambcorp.com>
2019-09-09 10:11:13 -0400

    Update ChkGUAPholdings.sh

--- ChkGUAPholdings.sh
+++ ChkGUAPholdings.sh
@@ -5,40 +5,39 @@ clear
 
 echo "test1 test2 test3"
 
-curl http://159.65.221.180:3001/ext/getbalance/GK6Vdaq4Gp7G5aUSPiMFb6gKpk3Sq5xeCs 2> /dev/null
+curl -s http://159.65.221.180:3001/ext/getbalance/GK6Vdaq4Gp7G5aUSPiMFb6gKpk3Sq5xeCs 2> /dev/null
 
-MN1= $(curl http://159.65.221.180:3001/ext/getbalance/GK6Vdaq4Gp7G5aUSPiMFb6gKpk3Sq5xeCs 2> /dev/null)
+#MN1= $(curl http://159.65.221.180:3001/ext/getbalance/GK6Vdaq4Gp7G5aUSPiMFb6gKpk3Sq5xeCs 2> /dev/null)
 
-echo "MN1 Total= $MN1"
+#echo "MN1 Total= $MN1"
+#MN2= $(curl http://159.65.221.180:3001/ext/getbalance/GPRN7jMA9DAKPaSThT9TpH3EsHfgG2zSg6 2> /dev/null)
 
-MN2= $(curl http://159.65.221.180:3001/ext/getbalance/GPRN7jMA9DAKPaSThT9TpH3EsHfgG2zSg6 2> /dev/null)
+#echo ""
+#echo "MN2 Total= $MN2"
 
-echo ""
-echo "MN2 Total= $MN2"
+#MN3= $(curl http://159.65.221.180:3001/ext/getbalance/GadrCsoWB1YoGKiAofte1c8iyNaZpn7DHR 2> /dev/null)
 
-MN3= $(curl http://159.65.221.180:3001/ext/getbalance/GadrCsoWB1YoGKiAofte1c8iyNaZpn7DHR 2> /dev/null)
+#echo ""
+#echo "MN3 Total= $MN3"
 
-echo ""
-echo "MN3 Total= $MN3"
+#MN4= $(curl http://159.65.221.180:3001/ext/getbalance/GVmzuuU8M2epADTHAFTcJfKKeVAT4odyZh 2> /dev/null)
 
-MN4= $(curl http://159.65.221.180:3001/ext/getbalance/GVmzuuU8M2epADTHAFTcJfKKeVAT4odyZh 2> /dev/null)
+#echo ""
+#echo "MN4 Total= $MN4"
 
-echo ""
-echo "MN4 Total= $MN4"
+#MN5=  $(curl http://159.65.221.180:3001/ext/getbalance/GTsz49S1dgbMcC82wcekku6kyHwNU97yYa 2> /dev/null)
 
-MN5=  $(curl http://159.65.221.180:3001/ext/getbalance/GTsz49S1dgbMcC82wcekku6kyHwNU97yYa 2> /dev/null)
+#echo ""
+#echo "MN5 Total= $MN5"
 
-echo ""
-echo "MN5 Total= $MN5"
+#MN6= $(curl http://159.65.221.180:3001/ext/getbalance/GNENXQeidRkFxfjjoRcNiLpfSRMvs5eY6d 2> /dev/null)
 
-MN6= $(curl http://159.65.221.180:3001/ext/getbalance/GNENXQeidRkFxfjjoRcNiLpfSRMvs5eY6d 2> /dev/null)
+#echo ""
+#echo "MN6 Total= $MN6"
 
-echo ""
-echo "MN6 Total= $MN6"
+#echo ""
+#echo "______________________________________________"
 
-echo ""
-echo "______________________________________________"
+#MNTotal= $MN1 + $MN2 + $MN3 + $MN4 + $MN5 + $MN6
 
-MNTotal= $MN1 + $MN2 + $MN3 + $MN4 + $MN5 + $MN6
-
-echo "Total GUAP Holdings for all MNs: $MNTotal"
+#echo "Total GUAP Holdings for all MNs: $MNTotal"
