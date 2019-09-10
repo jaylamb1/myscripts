d8e719fa8fdbe6ed9f441769ba2cc20a5eb8b587
Jason Lamb <jlamb@lambcorp.com>
2019-09-10 15:59:05 -0400

    Update ChkGUAPholdingsv2.sh

--- ChkGUAPholdingsv2.sh
+++ ChkGUAPholdingsv2.sh
@@ -14,15 +14,11 @@ MNArray[$n]=$line
 n=$((n+1))
 done < $filename
 
-#echo ""
-#echo ${MNArray[*]}
-
-echo ""
 echo ""
 echo ""
 
 n=0
-echo "|------------- Adress -------------|-------------------------------|"
+echo "-------------- Adress ----------------------------------------------"
 for i in "${MNArray[@]}"
 do
 
@@ -31,24 +27,23 @@ do
   tempVar=${Addr[$n]}
 
   #echo " GK6Vdaq4Gp7G5aUSPiMFb6gKpk3Sq5xeCs GUAP Subtotal =      30,000.00"
-  echo " $i GUAP Subtotal =      $(python -c 'import os; print "{0:,.2f}".format(float(os.environ["tempVar"]))')"
+  echo " $i GUAP Subtotal =    $(python -c 'import os; print "{0:,.3f}".format(float(os.environ["tempVar"]))')"
   echo ""
   ((++n))
 done
 
-  echo "|__________________________________|_______________________________|"
+  echo "____________________________________________________________________"
 
 MN_Total=0
 n=0
 #Add everything up
 for i in "${Addr[@]}"
 do
-	#echo $i
   tempVar=${Addr[$n]}
   #MN_Total=$((0 + $(python -c 'import os; print "{0:,.2f}".format(float(os.environ["MN_Total"]))') + $(python -c 'import os; print "{0:,.2f}".format(float(os.environ[""]))')))
   #MN_Total=$(python -c 'import os; print "{:.2f}".format((float(os.environ["MN_Total"]) + float(os.environ["tempVar"]))')
   MN_Total=$(python -c 'import os; print "{:.3f}".format((float(os.environ["MN_Total"]) + float(os.environ["tempVar"])))')
-  echo $MN_Total
+  #echo $MN_Total
   #Addr[$n]=$(curl -s -X GET $parm)
 
   #echo "Address #$($n + 1) Total= $(python -c 'import os; print "{0:,.2f}".format(float(os.environ["tempVar"]))')"
@@ -68,16 +63,15 @@ GUAPTotal=$(curl -s -X GET $parm7)
 Perc=$(python -c 'import os; print "{:.2f}".format((float(os.environ["MN_Total"]) / float(os.environ["GUAPTotal"]) * 100))')
 
 #Perc=$(( '$MNTotal / $GUAPTotal' | bc -l ))
-
-echo "MNs Total= $(python -c 'import os; print "{0:,.3f}".format(float(os.environ["MN_Total"]))')"
 echo ""
+echo "Total GUAP Holdings = $(python -c 'import os; print "{0:,.3f}".format(float(os.environ["MN_Total"]))')"
 echo ""
 echo "Total GUAP Money Supply is: $(python -c 'import os; print "{0:,.5f}".format(float(os.environ["GUAPTotal"]))')"
 echo ""
 parm8="http://159.65.221.180:3001/ext/getmasternodecount"
 MNCount=$(curl -s -X GET $parm8)
-echo "Total number of GUAP masternodes: $MNCount"
-echo ""
 echo "Percentage of total GUAP money is: $Perc%"
+echo ""
+echo "Total number of GUAP masternodes: $MNCount"
 echo ""
 set +a
