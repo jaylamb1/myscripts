59a7f2f732bc3c6ce5f1a8f026b569cbc6c27c20
Jason Lamb <jlamb@lambcorp.com>
2019-09-10 20:58:18 -0400

    Update ChkGUAPholdingsv2.sh

--- ChkGUAPholdingsv2.sh
+++ ChkGUAPholdingsv2.sh
@@ -20,7 +20,7 @@ echo ""
 echo ""
 
 n=0
-echo "      [ Adress ]                                      [ Subtotal ]"
+echo "      [ Address ]                                     [ Subtotal ]"
 echo "--------------------------------------------------------------------"
 echo ""
 for i in "${MNArray[@]}"
