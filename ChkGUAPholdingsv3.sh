79a506398a189a96a1cd030fee03f0be0a037c5d
Jason Lamb <jlamb@lambcorp.com>
2019-09-13 16:35:04 -0400

    Update ChkGUAPholdingsv3.sh

--- ChkGUAPholdingsv3.sh
+++ ChkGUAPholdingsv3.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#This script should be called with text file as an argument:
+# e.g sudo /root/ChkGuapHoldingsv3.sh /root/file.text
+
+#The text file should have the GUAP addresses you want to check the amounts for and a corresponding label for each address
+#Format for the text should be:
+#     Label1<space>GUAP Address1
+#     Label2<space>GUAP Address2
+
 #Make all variable available as environment variables
 set -a
 
@@ -7,39 +15,41 @@ echo ""
 echo "                   [GUAP Holdings Snaphot]                       "
 echo "-----------------------------------------------------------------"
 
-# Print timestamp in Day Date(MM-DD-YYYY) Time(HH:MMam) format
+#Print timestamp in Day Date(MM-DD-YYYY) Time(HH:MMam) Timezone format
 d=$(TZ=":US/Eastern" date +'%a %m-%d-%Y %I:%M%P EST')
 echo "Timestamp : $d"
 echo ""
 
-
+#Create arrays to hold GUAP addresses and address labels from file
 declare -a MNArray
 declare -a MNLabelArray
 
+#capture the external file
 filename=$1
+
 #Clean up the file, remove bash comments and empty lines (creates a backup before removal)
 sed -i".bkup" 's/^#.*$//' $filename #remove comments
 sed -i '/^$/d' $filename #remove empty lines
 
+#While loop to read in each GUAP address and corresponding label
 n=0
 while read label address; do
 # reading each line
-#echo "$label $address"
 MNLabelArray[$n]=$label
 MNArray[$n]=$address
 n=$((n+1))
 done < $filename
 
-
-#echo ""
 echo ""
 
-n=0
+
 echo "[Label]      [Address]                                [Subtotal] "
 echo "-----------------------------------------------------------------"
 
 echo ""
 
+#For loop to get the current amount of GUAP in each saved GUAP address and print out each address, with its label and its GUAP amount
+n=0
 for i in "${MNArray[@]}"
 do
 
@@ -53,7 +63,7 @@ do
   ((++n))
 done
 
-
+#Var to hold the total amount of GUAP from all saved GUAP addresses, and For loop to iterate through MNArray and calculate the total of all the addresses
 MN_Total=0
 n=0
 #Add everything up
@@ -67,13 +77,15 @@ do
 done
 
 
-
+#Get total current GUAP chain money supply
 parm7="http://159.65.221.180:3001/ext/getmoneysupply"
 
 GUAPTotal=$(curl -s -X GET $parm7)
 
-
+#Get percentage of total GUAP money suppy held by the addressed evaluated
 Perc=$(python -c 'import os; print "{:>13,.2f}".format((float(os.environ["MN_Total"]) / float(os.environ["GUAPTotal"]) * 100))')
+
+#Print out total holding and total GUAP money supply
 echo "-----------------------------------------------------------------"
 echo "  Total GUAP Holdings                           : $(python -c 'import os; print "{0:>14,.3f}".format(float(os.environ["MN_Total"]))')"
 
@@ -83,19 +95,24 @@ echo ""
 echo "Total GUAP Money Supply                         : $(python -c 'import os; print "{0:>14,.3f}".format(float(os.environ["GUAPTotal"]))')"
 
 echo ""
+#Get total number of GUAP masternodes and do some formating
 parm8="http://159.65.221.180:3001/ext/getmasternodecount"
 MNCount=$(curl -s -X GET $parm8)
+
+#Removes random whitespace from beginning of MNCount
 shopt -s extglob
 MNCount=${MNCount##*( )}
 shopt -u extglob
+#right justify
 MNCount=$(printf '%14s' $MNCount)
 
 
-
+#Get current block count/height
 parm9="http://159.65.221.180:3001/api/getblockcount"
 BlockHeight=$(curl -s -X GET $parm9)
 BlockHeight=$(printf '%14s' $BlockHeight)
 
+#Print out percentage of GUAP money supply, Masternode count, and GUAP chain block count/height
 echo "Percentage of total GUAP Money Supply           : $Perc%"
 echo ""
 echo "Total number of GUAP masternodes                : $MNCount"
